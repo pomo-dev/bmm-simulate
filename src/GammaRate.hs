dcd2bb4fc7b746cb63edc6e8b8ff80e5d872790f
Dominik Schrempf <dominik.schrempf@gmail.com>
2017-08-24 12:02:36 +0100

    Implement calculation of gamma rates.

--- src/GammaRate.hs
+++ src/GammaRate.hs
@@ -18,6 +18,7 @@ Gamma rate heterogeneity. This module provides the necessary definitions.
 module GammaRate where
 
 -- import qualified Distribution                  as D
+import           Numeric.Integration.TanhSinh
 import           Statistics.Distribution
 import           Statistics.Distribution.Gamma
 
@@ -26,6 +27,30 @@ import           Statistics.Distribution.Gamma
 -- that represent the respective categories. Use the mean rate for each
 -- category.
 getGammaRatesMean :: Int -> Double -> [Double]
-getGammaRatesMean n alpha = quantiles
+getGammaRatesMean n alpha = means ++ lastMean
   where gamma = gammaDistr alpha (1.0/alpha)
         quantiles = [ quantile gamma (fromIntegral i / fromIntegral n) | i <- [0..n] ]
+        -- Calculate the mean rate. Multiplication with the number of rate
+        -- categories 'n' is necessary because in each n-quantile the
+        -- probability mass is 1/n.
+        meanFunc x = fromIntegral n * x * density gamma x
+        -- Only calculate the first (n-1) categories with normal integration.
+        means = [ intAToB meanFunc (quantiles !! i) (quantiles !! (i+1)) | i <- [0..n-2] ]
+        -- The last category has to be calculated with an improper integration.
+        lastMean = [intAToInf meanFunc (quantiles !! (n-1))]
+
+-- | The error of integration.
+eps :: Double
+eps = 1e-6
+
+-- | The integration method to use
+method :: (Double -> Double ) -> Double -> Double -> [Result]
+method = parSimpson
+
+-- | Helper function for a normal integral from 'a' to 'b'.
+intAToB :: (Double -> Double) -> Double -> Double -> Double
+intAToB f a b = result . absolute eps $ method f a b
+
+-- | Helper function for an improper integral from 'a' to infinity.
+intAToInf :: (Double -> Double) -> Double -> Double
+intAToInf f a = (result . absolute eps $ nonNegative method f) - intAToB f 0 a
